b77214525b1f5b3486a612b7d78f2428b5b01560
jeff-goldstein <jeff.goldstein@sparkpost.com>
2016-09-06 22:42:27 -0700

    Update SparkPostKey.php

--- SparkPostCampaignSubmissionCode/SparkPostKey.php
+++ SparkPostCampaignSubmissionCode/SparkPostKey.php
@@ -38,13 +38,33 @@ h4 {
     font-weight: bold;
     color: #298272;
 }
+
+input[type=text] {
+    width: 130px;
+    box-sizing: border-box;
+    border: 8px solid #black;
+    border-radius: 4px;
+    font-size: 12px;
+    background-color: white;
+    background-position: 10px 10px;
+    background-repeat: no-repeat;
+    padding: 2px 2px 2px 4px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+    width: 300px;
+    border: 3px solid #555;
+}
 </style>
+
 <body id="bkgrnd">
 <h1><center> SparkPostMail Simple UI Campaign Generator </center></h1>
-<form action="SparkPostSubmit.php">
+<form name=keyform action="SparkPostSubmit.php">
 
 <h3>Your SparkPost API Key:</h3>
-  <input name="apikey" type="text" required><br><br>
+  <input name="apikey" type="text" required placeholder="API Key.."><br><br>
 <input type="submit" value="Submit" STYLE="color: #FFFFFF; font-family: Verdana; font-weight: bold; font-size: 12px; background-color: #72A4D2;" size="10" >
 </form>
 <br><br>
