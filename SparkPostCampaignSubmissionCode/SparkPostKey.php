3eb8427c39e62308d117d5afc4df23a54186cd24
jeff-goldstein <jeff.goldstein@sparkpost.com>
2016-09-20 15:03:00 -0700

    Tool Usage Warning and Github location

--- SparkPostCampaignSubmissionCode/SparkPostKey.php
+++ SparkPostCampaignSubmissionCode/SparkPostKey.php
@@ -12,20 +12,21 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. -->
 <!DOCTYPE html>
-<html><head>
- <title>SparkPost Campaign Submission</title>
- <link rel="shortcut icon" href="https://dl.dropboxusercontent.com/u/4387255/Tools.ico" type="image/vnd.microsoft.icon" />
-</head>
-<style>
-/* Set Background to SparkPost Flames */
-#bkgnd {
+<html>
+<head>
+    <title>SparkPost Campaign Submission</title>
+    <link href="https://dl.dropboxusercontent.com/u/4387255/Tools.ico" rel=
+    "shortcut icon" type="image/vnd.microsoft.icon">
+    <style>
+    /* Set Background to SparkPost Flames */
+    #bkgnd {
     background-image: url(https://dl.dropboxusercontent.com/u/4387255/bwback.jpg);
     background-position: right bottom, left top;
     background-repeat: no-repeat;
     padding: 15px;
-}
+    }
 
-h2 {
+    h2 {
     display: block;
     font-size: 1.5em;
     margin-top: 0.83em;
@@ -33,9 +34,9 @@ h2 {
     margin-left: 0;
     margin-right: 0;
     font-weight: bold;
-}
+    }
 
-h3 {
+    h3 {
     display: block;
     font-size: 1.17em;
     margin-top: 1em;
@@ -44,9 +45,9 @@ h3 {
     margin-right: 0;
     font-weight: bold;
     color: #298272;
-}
+    }
 
-h4 { 
+    h4 { 
     display: block;
     margin-top: 1.33em;
     margin-bottom: 1.33em;
@@ -54,10 +55,10 @@ h4 {
     margin-right: 0;
     font-weight: bold;
     color: #298272;
-}
+    }
 
-/* Expandable text fields when active */
-input[type=text] {
+    /* Expandable text fields when active */
+    input[type=text] {
     width: 130px;
     box-sizing: border-box;
     border: 8px solid #black;
@@ -69,23 +70,56 @@ input[type=text] {
     padding: 2px 2px 2px 4px;
     -webkit-transition: width 0.4s ease-in-out;
     transition: width 0.4s ease-in-out;
-}
+    }
 
-input[type=text]:focus {
+    input[type=text]:focus {
     width: 300px;
     border: 3px solid #555;
-}
-</style>
+    }
+    </style>
+</head>
+<body id="bkgnd">
+    <center>
+        <h1>SparkPostMail Simple UI Campaign Generator</h1>
+    </center>
+    <form action="SparkPostSubmit.php" id="keyform" name="keyform">
+        <h3>Your SparkPost API Key:</h3><input name="apikey" placeholder=
+        "API Key.." required="" type="text"><br>
+        <br>
+        <input size="10" style=
+        "color: #FFFFFF; font-family: Verdana; font-weight: bold; font-size: 12px; background-color: #72A4D2;"
+        type="submit" value="Submit">
+    </form><br>
+    <br>
+    Note: These API Keys can be created from the one of the admin pages on your
+    SparkPost account at <a href=
+    "https://app.sparkpost.com/account/credentials">https://app.sparkpost.com/account/credentials</a>.<br>
 
-<body id="bkgrnd">
-<h1><center> SparkPostMail Simple UI Campaign Generator </center></h1>
-<form name=keyform action="SparkPostSubmit.php">
-
-<h3>Your SparkPost API Key:</h3>
-  <input name="apikey" type="text" required placeholder="API Key.."><br><br>
-<input type="submit" value="Submit" STYLE="color: #FFFFFF; font-family: Verdana; font-weight: bold; font-size: 12px; background-color: #72A4D2;" size="10" >
-</form>
-<br><br>
-Note: These API Keys can be created from the one of the admin pages on your SparkPost account at <a href="https://app.sparkpost.com/account/credentials" >https://app.sparkpost.com/account/credentials</a>.  <br>At a minimum, you need to select 'Recipient Lists: Read/Write, Templates: Read/Write, and Transmissions:Read/Write'.
+    At a minimum, you need to select 'Recipient Lists: Read/Write, Templates:
+    Read/Write, and Transmissions:Read/Write'.
+    <center>
+        <table border="0" height=100 width="50%">
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+    </center>
+    <center>
+        <table border="1" style="background-color:#F5870A" width="50%">
+            <tr>
+                <td>
+                    This tool is free to use at your own risk and should be up
+                    and running at all times but is not monitored for uptime.
+                    The Campaign Generator DOES NOT hold any of your
+                    information, it simply uses your API key to obtain enough
+                    information in order to create, schedule or cancel your
+                    campaigns. The code behind this site is available from my
+                    Github repository at: <a href=
+                    "https://github.com/jeff-goldstein/sparkpost/tree/master/SparkPostCampaignSubmissionCode" target="_blank">
+                    https://github.com/jeff-goldstein/sparkpost/tree/master/SparkPostCampaignSubmissionCode</a>.
+                </td>
+            </tr>
+        </table>
+    </center>
 </body>
 </html>
